9cdaa954e85ac8a6e2b502d07fd261a952c5e29b
SafarGo <safaryan.ga@gmail.com>
2026-09-17 20:38:02 +0300

    Initial + added base movement commit

---
+++ DrinkOrDie/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+public class PlayerController : MonoBehaviour 
+{
+    public int Hp; 
+    [SerializeField] private float speed; 
+    private Rigidbody2D rb; 
+    private Vector2 movement; 
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    } 
+    private void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal"); 
+        movement.y = Input.GetAxisRaw("Vertical"); 
+        movement = movement.normalized; 
+    } 
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime); 
+    } 
+}
